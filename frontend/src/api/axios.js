c9f3ea0b28f187716ca92afdbd22968a488aa99b
svk091 <vamsiks091@gmail.com>
2024-11-10 11:56:26 +0530

    fixed axios instance

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseUrl: import.meta.env.VITE_SERVER_URL
+  baseURL: import.meta.env.VITE_SERVER_URL
 })
 
 export default axiosInstance
